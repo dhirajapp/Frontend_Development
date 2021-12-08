cee4963869c3db5168f6b2a3dabcbf97898c0030
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-12-08 23:32:46 +0530

    05 commit

---
+++ Module_001/Week_001/Basic_DSA/conditional_Statements/JS_02.js
@@ -0,0 +1,144 @@
+// if 
+// else if 
+// else if 
+// else if 
+// else 
+
+// let tempInDegree = 50;
+
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("It is cold outside");
+// }else if(tempInDegree < 25){
+//     console.log("wheather is okay ");
+// }else if(tempInDegree < 35){
+//     console.log("lets go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC");
+// }else{
+//     console.log("too hot!!");
+// }
+
+// console.log("hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let tempInDegree = 50;
+
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("It is cold outside");
+// }else if(tempInDegree < 25){
+//     console.log("wheather is okay ");
+// }else if(tempInDegree < 35){
+//     console.log("lets go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC");
+// }else{
+//     console.log("too hot!!");
+// }
+
+// console.log("hello there");
+
+
+
+let tempInDegree = 4;
+
+if(tempInDegree > 40){
+    console.log("too hot");
+}else if(tempInDegree > 30){
+    console.log("lets go for swim");
+}else if(tempInDegree > 20){
+    console.log("weather is cool");
+}else if(tempInDegree > 10){
+    console.log("it is very cold outside");
+}else{
+    console.log("extremely cold");
+}
+
+console.log("hello");
+
+// switch statement 
+
+
+// let day = 7; 
+
+
+// if(day === 0){
+//     console.log("Sunday");
+// }else if(day ===1){
+//     console.log("Monday");
+// }else if(day ===2){
+//     console.log("Tuesday");
+// }else if(day ===3){
+//     console.log("Wednesday");
+// }else if(day ===4){
+//     console.log("Thrusday");
+// }else if(day ===5){
+//     console.log("Friday");
+// }else if(day ===6){
+//     console.log("Saturday");
+// }else{
+//     console.log("Invalid Day");
+// }
+
+
+let day = 9;
+
+switch(day){
+    case 0:
+        console.log("Sunday");
+        break; 
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid Day");
+}
+
+
+
+// while loop 
+
+// 0 se 9 
+// dry don't repeat yourself
+let i = 0; // 1 2 3 4
+
+while(i<=9){
+    console.log(i);
+    i++;
+}
+console.log(`current value of i is ${i}`);
+console.log("hello");
