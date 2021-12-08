3981b0d90265da0d05d910e20a52dc496c628643
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-12-08 23:02:39 +0530

    01 commit

--- Module_001/Week_001/Basic_DSA/conditional_Statements/index.js
+++ Module_001/Week_001/Basic_DSA/conditional_Statements/index.js
@@ -3,5 +3,67 @@
 //-- conditional Statements like 
 //-- if, else and nested if & else .
 
+// console.log can print something on console 
+console.log("hello world");
+
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "Harshit";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "Mohit";
+
+console.log(firstName);
+
+
+// rules for naming variables 
+
+// you cannot start with number 
+// example :- 
+// 1value (invalid)
+// value1 (valid)
+
+var value1 = 2;
+console.log(value1);
+
+// you can use only undersore _ or dollar symbol 
+// first_name (valid)
+// _firstname (valid) 
+
+// first$name (valid)
+// $firstname (valid)
+
+// you cannot use spaces 
+// var first_name = "harshit"; // snake case writing 
+// var firstName = "harshit"; // camel case writing 
+// first name (invalid)
+
+// convention 
+// start with small letter and use camelCase 
+
+// let keyword 
+// declare variable with let keyword 
+
+let firstName = "harshit";
+firstName = "Mohit";
+console.log(firstName);
+
+
+// declare constants 
+
+const pi = 3.14;
+console.log(pi);
+
 
 
