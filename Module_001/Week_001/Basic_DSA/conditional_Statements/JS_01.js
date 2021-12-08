68f3f916c222fc4de35f974f0bf05055685bb797
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-12-08 23:22:48 +0530

    04 commit

---
+++ Module_001/Week_001/Basic_DSA/conditional_Statements/JS_01.js
@@ -0,0 +1,90 @@
+//Problem solving using 
+//-- conditional Statements like 
+//-- if, else and nested if & else .
+
+// if else condition 
+
+// let age = 17;
+
+// if(age>=18){
+//     console.log("User can play ddlc");
+// }else {
+//     console.log("User can play mario");
+// }
+
+// let num = 13;
+
+// if(num%2===0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+// falsy values 
+
+// ternary operator 
+
+// let age = 4;
+// let drink;
+
+// if(age>=5){
+//     drink = "coffee";
+// }else{
+//     drink = "milk";
+// }
+
+// console.log(drink);
+
+// ternary operator / conditional operator 
+
+// let age = 3;
+// let drink = age >= 5 ? "coffee" : "milk";
+// console.log(drink);
+
+// and  or operator 
+
+
+
+// if(firstName[0] === "H"){
+//     console.log("your name starts with H")
+// }
+
+// if(age > 18){
+//     console.log("you are above 18");
+// }
+
+// if(firstName[0] === "H" && age>18){
+//     console.log("Name starts with H and above 18");
+// }else{
+//     console.log("inside else");
+// }
+let firstName = "arshit";
+let age = 16;
+
+if(firstName[0] === "H" || age>18){
+    console.log("inside if");
+}else{
+    console.log("inside else");
+}
+
+// nested if else
+
+// winning number 19 
+
+// 19 your guess is right 
+// 17 too low 
+// 20 too high 
+
+let winningNumber = 19;
+let userGuess = +prompt("Guess a number");
+
+if(userGuess === winningNumber){
+    console.log("Your guess is right!!");
+}else{
+    if(userGuess < winningNumber){
+        console.log("too low !!!");
+    }else{
+        console.log("too high !!!");
+    }
+}
+
